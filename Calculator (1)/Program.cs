5fc8e5c8ae6bfa8a323025912c568645ef9d5771
harishchan2010 <89344472+harishchan2010@users.noreply.github.com>
2021-08-22 18:36:44 +0530

    Updating sample.

--- Calculator (1)/Program.cs
+++ Calculator (1)/Program.cs
@@ -9,7 +9,7 @@ namespace Calculator__1_
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///  The main entry point for the application. This is sample edit. 
         /// </summary>
         [STAThread]
         static void Main()
